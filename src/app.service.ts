c7eea1e71434c29d6f5383fa650c2170dc8cbdf7
cikada3301 <56870747+cikada3301@users.noreply.github.com>
2023-12-01 01:43:30 +0300

    Add files via upload

--- src/app.service.ts
+++ src/app.service.ts
@@ -20,9 +20,7 @@ export class AppService {
 
         if (response.status === 204) {
             await this.createContact(name, email, phone);
-        }
-
-        else {
+        } else {
             const id = response.data._embedded.contacts[0].id;
 
             await this.updateContact(name, email, phone, id);
@@ -62,6 +60,17 @@ export class AppService {
                 }
             }
         ).toPromise().then(result => console.log(result));
+
+        const contactId = await this.http.get('https://vladpalagin2013.amocrm.ru/api/v4/contacts', {
+            params: {
+                query: phone
+            },
+            headers: {
+                Authorization: `Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImp0aSI6ImUwN2I2YmY5NGY3MTU3ZjFhZjYyYmM3YmVlMzAzNzBmOTg0YzdjMjNhNDcwYzY1NzgzYzJhZDFhMGRiOGVlY2I1MWMwNDdlNzllM2Y3YjYyIn0.eyJhdWQiOiJmOTBiMWEzZC03MTYwLTQ4YTYtOTc0Yy0zYTE5ZjIzZjI1NTYiLCJqdGkiOiJlMDdiNmJmOTRmNzE1N2YxYWY2MmJjN2JlZTMwMzcwZjk4NGM3YzIzYTQ3MGM2NTc4M2MyYWQxYTBkYjhlZWNiNTFjMDQ3ZTc5ZTNmN2I2MiIsImlhdCI6MTcwMTM0NjI4MiwibmJmIjoxNzAxMzQ2MjgyLCJleHAiOjE3MDE0MzI2ODIsInN1YiI6IjEwMzk4Mjc0IiwiZ3JhbnRfdHlwZSI6IiIsImFjY291bnRfaWQiOjMxNDM0NjgyLCJiYXNlX2RvbWFpbiI6ImFtb2NybS5ydSIsInZlcnNpb24iOjIsInNjb3BlcyI6WyJwdXNoX25vdGlmaWNhdGlvbnMiLCJmaWxlcyIsImNybSIsImZpbGVzX2RlbGV0ZSIsIm5vdGlmaWNhdGlvbnMiXX0.Dua9HDWwc6JhKGpdTCb56mfsD2UzgSycc5rCCNVrX0QkzX95FJC8sr1KeygO5rvQoasPBWqVwHbIbnvrQXPSdywyKfBMGR9h53e9AQEcUTILXENsawuQH7PG_oSZrBMlCsoxkT87-rmM8_c8ExRGsknB6BEKLKzmG--HonUbDLkFiGUCvGQWGIRLYJlx24bdZ8v9IevAtd4EaGgf4voEHKzJvwdFuqglMvbiyTht34YtCONr8lwJgElP05idSQxKbMEoKsjffyyVnaoX5L-KCAZDVSIE-ZvpWmA5wNNpVfLjLYlJDOqB_3URRnkhpDWO5Jk6KjAQmCGFOkTfrggv1Q`
+            }
+        }).toPromise();
+
+        await this.createLead(contactId.data._embedded.contacts[0].id);
     }
 
     async updateContact(name: string, email: string, phone: string, id: number) {
@@ -92,6 +101,34 @@ export class AppService {
 
         await this.http.patch('https://vladpalagin2013.amocrm.ru/api/v4/contacts',
             [contact],
+            {
+                headers: {
+                    Authorization: `Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImp0aSI6ImUwN2I2YmY5NGY3MTU3ZjFhZjYyYmM3YmVlMzAzNzBmOTg0YzdjMjNhNDcwYzY1NzgzYzJhZDFhMGRiOGVlY2I1MWMwNDdlNzllM2Y3YjYyIn0.eyJhdWQiOiJmOTBiMWEzZC03MTYwLTQ4YTYtOTc0Yy0zYTE5ZjIzZjI1NTYiLCJqdGkiOiJlMDdiNmJmOTRmNzE1N2YxYWY2MmJjN2JlZTMwMzcwZjk4NGM3YzIzYTQ3MGM2NTc4M2MyYWQxYTBkYjhlZWNiNTFjMDQ3ZTc5ZTNmN2I2MiIsImlhdCI6MTcwMTM0NjI4MiwibmJmIjoxNzAxMzQ2MjgyLCJleHAiOjE3MDE0MzI2ODIsInN1YiI6IjEwMzk4Mjc0IiwiZ3JhbnRfdHlwZSI6IiIsImFjY291bnRfaWQiOjMxNDM0NjgyLCJiYXNlX2RvbWFpbiI6ImFtb2NybS5ydSIsInZlcnNpb24iOjIsInNjb3BlcyI6WyJwdXNoX25vdGlmaWNhdGlvbnMiLCJmaWxlcyIsImNybSIsImZpbGVzX2RlbGV0ZSIsIm5vdGlmaWNhdGlvbnMiXX0.Dua9HDWwc6JhKGpdTCb56mfsD2UzgSycc5rCCNVrX0QkzX95FJC8sr1KeygO5rvQoasPBWqVwHbIbnvrQXPSdywyKfBMGR9h53e9AQEcUTILXENsawuQH7PG_oSZrBMlCsoxkT87-rmM8_c8ExRGsknB6BEKLKzmG--HonUbDLkFiGUCvGQWGIRLYJlx24bdZ8v9IevAtd4EaGgf4voEHKzJvwdFuqglMvbiyTht34YtCONr8lwJgElP05idSQxKbMEoKsjffyyVnaoX5L-KCAZDVSIE-ZvpWmA5wNNpVfLjLYlJDOqB_3URRnkhpDWO5Jk6KjAQmCGFOkTfrggv1Q`
+                }
+            }
+        ).toPromise().then(result => console.log(result));
+
+        await this.createLead(id);
+    }
+
+    async createLead(id: number) {
+
+        const lead =
+            {
+                "name": "Название сделки",
+                "price": 0,
+                "_embedded": {
+                    "contacts": [
+                        {
+                            "id": id,
+                            "is_main": true
+                        }
+                    ],
+                },
+            };
+
+        await this.http.post('https://vladpalagin2013.amocrm.ru/api/v4/leads',
+            [lead],
             {
                 headers: {
                     Authorization: `Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImp0aSI6ImUwN2I2YmY5NGY3MTU3ZjFhZjYyYmM3YmVlMzAzNzBmOTg0YzdjMjNhNDcwYzY1NzgzYzJhZDFhMGRiOGVlY2I1MWMwNDdlNzllM2Y3YjYyIn0.eyJhdWQiOiJmOTBiMWEzZC03MTYwLTQ4YTYtOTc0Yy0zYTE5ZjIzZjI1NTYiLCJqdGkiOiJlMDdiNmJmOTRmNzE1N2YxYWY2MmJjN2JlZTMwMzcwZjk4NGM3YzIzYTQ3MGM2NTc4M2MyYWQxYTBkYjhlZWNiNTFjMDQ3ZTc5ZTNmN2I2MiIsImlhdCI6MTcwMTM0NjI4MiwibmJmIjoxNzAxMzQ2MjgyLCJleHAiOjE3MDE0MzI2ODIsInN1YiI6IjEwMzk4Mjc0IiwiZ3JhbnRfdHlwZSI6IiIsImFjY291bnRfaWQiOjMxNDM0NjgyLCJiYXNlX2RvbWFpbiI6ImFtb2NybS5ydSIsInZlcnNpb24iOjIsInNjb3BlcyI6WyJwdXNoX25vdGlmaWNhdGlvbnMiLCJmaWxlcyIsImNybSIsImZpbGVzX2RlbGV0ZSIsIm5vdGlmaWNhdGlvbnMiXX0.Dua9HDWwc6JhKGpdTCb56mfsD2UzgSycc5rCCNVrX0QkzX95FJC8sr1KeygO5rvQoasPBWqVwHbIbnvrQXPSdywyKfBMGR9h53e9AQEcUTILXENsawuQH7PG_oSZrBMlCsoxkT87-rmM8_c8ExRGsknB6BEKLKzmG--HonUbDLkFiGUCvGQWGIRLYJlx24bdZ8v9IevAtd4EaGgf4voEHKzJvwdFuqglMvbiyTht34YtCONr8lwJgElP05idSQxKbMEoKsjffyyVnaoX5L-KCAZDVSIE-ZvpWmA5wNNpVfLjLYlJDOqB_3URRnkhpDWO5Jk6KjAQmCGFOkTfrggv1Q`
